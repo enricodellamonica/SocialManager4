5d91f0887c142d4bedf92de93bf870991a28fc84
enricodellamonica <enricodellamonica@gmail.com>
2015-07-20 16:37:15 +0200

    dettagli profilo e lista amici

---
+++ SocialManager4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SocialManager4.MvcApplication" Language="C#" %>
---
+++ SocialManager4/Models/FacebookPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using SocialManager4.Attributes;
+
+namespace SocialManager4.Models
+{
+    public class FacebookPageViewModel
+    {
+        [Required]
+        [FacebookMapping("id")]
+        public string Id { get; set; }
+
+        [Required]
+        [FacebookMapping("name")]
+        public string Name { get; set; }
+
+        [FacebookMapping("link")]
+        public string PageURL { get; set; }
+
+        [Display(Name = "# Likes")]
+        [FacebookMapping("likes")]
+        public long Likes { get; set; }
+
+        [Display(Name = "# Talking")]
+        [FacebookMapping("talking_about_count")]
+        public long TalkingCount { get; set; }
+
+        [Display(Name = "Visible")]
+        [FacebookMapping("is_published")]
+        public bool Published { get; set; }
+
+    }
+}
